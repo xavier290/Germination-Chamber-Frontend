a3ba64ff287c24b7fac2a863a40738884c1bc039
xavier290 <xa25alex@gmail.com>
2024-06-27 10:55:40 -0600

    login page updated

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,10 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import Logo from '../images/logo2.png';
-
+import Warning from '../images/warning.png';
 
 const Login = () => {
     const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
@@ -21,15 +22,22 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-          const response = await axios.post('https://squid-app-ht2cp.ondigitalocean.app/api/auth/login', { email, password });
-          localStorage.setItem('token', response.data.token);
-          localStorage.setItem('name', response.data.name);
-          navigate('/dashboard');
+            const response = await axios.post('https://squid-app-ht2cp.ondigitalocean.app/api/auth/login', { email, password });
+            // console.log(response.data);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('name', response.data.name);
+
+            navigate('/dashboard');
         } 
         catch (error) {
-          console.error('Login failed', error);
+            console.error('Login failed', error);
+            setErrorMessage('Correo o contraseña incorrectos. Por favor, inténtelo de nuevo.');
           // Handle login failure (e.g., show error message)
         }
+    };
+
+    const clearErrorMessage = () => {
+        setErrorMessage('');
     };
     
     return (
@@ -39,6 +47,20 @@ const Login = () => {
                 <p>SmartClimate</p>
             </div>
             <div className="pageContent">
+                {errorMessage && (
+                    <div className="Message">
+                        <div className="content">
+                            <div className="img">
+                                 <img src={Warning} alt="warning sign" />
+                            </div>
+                            <div className="payload">
+                                <p className='boldtext'>Parece que algo salio mal...</p>
+                                <p>{errorMessage}</p>
+                                <button onClick={clearErrorMessage}>&times;</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
                 <form onSubmit={handleLogin}>
                     <h1>Iniciar Sesión</h1>
                     
